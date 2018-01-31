6b50fe068c49ca17e218417969521fc96ef09291
Bogdan Sitaru <bogdan10bos@hotmail.com>
2018-01-31 11:07:49 +0200

    Added Runner.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Runner.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Runner {
+    private DcMotor m1, m2, m3, m4;
+
+    Runner (DcMotor _m1, DcMotor _m2, DcMotor _m3, DcMotor _m4)
+    {
+        m1 = _m1;
+        m2 = _m2;
+        m3 = _m3;
+        m4 = _m4;
+    }
+}
