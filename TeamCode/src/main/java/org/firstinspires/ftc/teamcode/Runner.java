cf00c5f996db28501614fb0aa46dca8c910aecbd
Bogdan Sitaru <bogdan10bos@hotmail.com>
2018-01-31 11:11:46 +0200

    Improved Runner.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Runner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Runner.java
@@ -3,13 +3,13 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Runner {
-    private DcMotor m1, m2, m3, m4;
+    private DcMotor leftF, leftB, rightF, rightB;
 
     Runner (DcMotor _m1, DcMotor _m2, DcMotor _m3, DcMotor _m4)
     {
-        m1 = _m1;
-        m2 = _m2;
-        m3 = _m3;
-        m4 = _m4;
+        leftF = _m1;
+        leftB = _m2;
+        rightF = _m3;
+        rightB = _m4;
     }
 }
