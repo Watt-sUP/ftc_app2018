afbbef58213d6845817f3990af9674ff9fe2ba9c
tavi <tavsipeed@gmail.com>
2018-03-03 03:34:22 -0800

    modified sensor stuff, place cube

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor_Stuff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor_Stuff.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 /**
- * Created by Tavi on 2/21/2018.
+ * Created by gaY  on 2/21/2018.
  */
 
 public class Sensor_Stuff {
@@ -26,27 +26,37 @@ public class Sensor_Stuff {
         rnr = x;
 
     }
-    private void  Keep_Orientation  (int Optimal_pos ){
-        while( gyro.getHeading() != Optimal_pos )
+    /**
+     *KEEP_ORIENTATION
+     * makes sure the robot keeps wanted position
+     * @param Optimal_pos optimal heading
+     */
+
+    private void  Keep_Orientation  (int Optimal_pos ) {
+        while (gyro.getHeading() != Optimal_pos)
             // test to rotate to the smaller angle
-                //ratio
+            //ratio
             //ToDo: test if  rotation constant is big enough for rotation to be made when Optimal_pos-gyro.getHeading is small, try a bigger/smaller constant
-         if( Optimal_pos-gyro.getHeading() > 0 )
-            rnr.setPower(-( Optimal_pos - gyro.getHeading() ) * 0.72 ,-( Optimal_pos - gyro.getHeading() ) * 0.72);//proportional rotation
-            else if( Optimal_pos-gyro.getHeading() < 0 )
-             rnr.setPower(-( Optimal_pos - gyro.getHeading() ) * 0.72 ,( Optimal_pos - gyro.getHeading() ) * 0.72);//proportional rotation
-        }
+            if (Optimal_pos - gyro.getHeading() > 0)
+                rnr.setPower(-(Optimal_pos - gyro.getHeading()) * 0.072, -(Optimal_pos - gyro.getHeading()) * 0.072);//proportional rotation
+            else if (Optimal_pos - gyro.getHeading() < 0)
+                rnr.setPower(-(Optimal_pos - gyro.getHeading()) * 0.072, (Optimal_pos - gyro.getHeading()) * 0.072);//proportional rotation
+    }
 
     /**
-     // place cube where you need to
+     * PLACE_CUBE
+     // places cube where you need to
      * @param drawer_target_pos specifies where the cube will be placed 1-right, 2-centre, 3-right
      * ToDo: test to set drawer_target_pos type to vumark types instead of int, so the function to be called straight with the vumark output
      *ToDo: set distance to look for drawer
      */
     private void Place_Cube  ( int drawer_target_pos) {
         int nr = 0;
+        double last_dist=dist_s.getDistance(DistanceUnit.CM);
+
         while ( true ) {
-            if ( dist_s.getDistance( DistanceUnit.CM ) < 10 && !dist_s_Target )
+
+            if (last_dist- dist_s.getDistance( DistanceUnit.CM )>=7 && !dist_s_Target )
             {
                 dist_offset=false;
                 dist_s_Target = true;
@@ -58,7 +68,7 @@ public class Sensor_Stuff {
                 }
 
             
-            if ( dist_s.getDistance(DistanceUnit.CM) > 10 && !dist_offset ) dist_offset = true;
+            if (last_dist- dist_s.getDistance( DistanceUnit.CM )>=7 && !dist_offset ) dist_offset = true;
             if ( dist_offset ) dist_s_Target = false;
             }
 
