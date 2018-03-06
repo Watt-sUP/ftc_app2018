044a58ad09f3b097de79b0b84245be915923a952
Bogdan Sitaru <bogdan10bos@hotmail.com>
2018-03-06 10:24:30 +0200

    Added RGB Strip. :)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RGBStrip.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+/**
+ * RGBStrip class is used to controll the RGB strip attached to robot. :) (#Duta)
+ */
+public class RGBStrip
+{
+    private DcMotor plus, red, green, blue;
+
+    private Telemetry.Item telemetry;
+    private boolean verbose = false;
+
+    RGBStrip(DcMotor _plus, DcMotor _red, DcMotor _green, DcMotor _blue, Object... _telemetry)
+    {
+        plus = _plus; red = _red; green = _green; blue = _blue;
+        plus.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        red.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        green.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        blue.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        if(_telemetry.length > 0 && (_telemetry[0] instanceof Telemetry.Item))
+        {
+            verbose = true;
+            telemetry = (Telemetry.Item) _telemetry[0];
+        }
+        else
+            verbose = false;
+    }
+
+    public void off()
+    {
+        plus.setPower(0.0);
+        red.setPower(0.0);
+        green.setPower(0.0);
+        blue.setPower(0.0);
+    }
+
+    public void setColor(int r, int g, int b)
+    {
+        if(r == 0 && g == 0 && b == 0)
+        {
+            off();
+            return;
+        }
+
+        double rPower = (double)r / (double)255;
+        double gPower = (double)g / (double)255;
+        double bPower = (double)b / (double)255;
+
+        if(r == 0)  rPower = -0.01;
+        if(g == 0)  gPower = -0.01;
+        if(b == 0)  bPower = -0.01;
+
+        plus.setPower(0.01);
+        red.setPower(rPower);
+        green.setPower(gPower);
+        blue.setPower(bPower);
+    }
+
+    public void logInformation()
+    {
+        telemetry.setValue( String.format("%.3f", plus.getPower()) + " " + String.format("%.3f", red.getPower()) + " " +
+                String.format("%.3f",green.getPower()) + " " + String.format("%.3f", blue.getPower()) );
+    }
+}
